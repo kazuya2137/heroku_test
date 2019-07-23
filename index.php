8608af95c8107dca21ebd3d7973c586f5e5e951b
和也山本 <kazuya2137@gmail.com>
2019-07-23 14:14:16 +0800

    ホゲホゲ

---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>かずやの自己紹介サイト</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="ここにサイト説明を入れます">
+<meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/style-opening.css">
+<link rel="stylesheet" href="css/lightbox.css">
+<script src="js/fixmenu_pagetop.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/scroll.js"></script>
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!--[if lt IE 10]>
+<style>
+#menubar {position:absolute;bottom:10%;}
+#menubar li {width:15%;float:left;margin-left:3%;position:relative;}
+#menubar li:hover {width:15%;}
+</style>
+<![endif]-->
+</head>
+
+<body>
+
+<header>
+
+<h1>かずやの自己紹介サイト</h1>
+
+<ul id="menubar">
+<li id="menu1"><a href="#news"><img src="images/img1.jpg" alt="">自己紹介</a></li>
+<li id="menu2"><a href="#about"><img src="images/img2.jpg" alt="">趣味</a></li>
+<li id="menu3"><a href="#gallery"><img src="images/img3.jpg" alt="">写真集</a></li>
+<li id="menu4"><a href="#link"><img src="images/img4.jpg" alt="">リンク</a></li>
+<li id="menu5"><a href="#contact"><img src="images/img5.jpg" alt=""><お問い合わせ</a></li>
+</ul>
+
+</header>
+
+<div class="contents" id="news">
+
+    <div class="inner">
+
+    <section>
+
+    <h2>自己紹介</h2>
+
+    <h3>山本和也</h3>
+    <p>僕の名前は山本和也です。広島出身の22歳です。この留学に来た動機は学校が終わってアメリカに行きたかったのでアメリカに2ヶ月間いる間に今のままだとここにいても自分でできることがわかってしまって、それをわかった上でまだアメリカにいるのはもったいないと思っていて希望しました。学生時代に貯金してたお金も全部使ってきたので本当に計画もなくここにきています。</p>
+    </section>
+
+    <section>
+
+    <h2>セルフコーチング</h2>
+
+    <dl id="new">
+    <dt>20XX/XX/XX</dt>
+    <dd>周りに流されない生き方をする。尚且つ周りの意見も聞く。</dd>
+    <dt>20XX/XX/XX</dt>
+    <dd>今を一生懸命生きて、何事にも真剣に取り組む。</dd>
+    <dt>2019/10</dt>
+    <dd>起業をする。</dd>
+    <dt>2022/7</dt>
+    <dd>ビリオネアになる。</dd>
+    <dt>2023/7</dt>
+    <dd>恵まれない子を助ける</dd>
+    
+    </dl>
+    
+    </section>
+
+    </div>
+    <!--/.inner-->
+
+</div>
+<!--/.contents-->
+
+
+<div class="contents bg1" id="about">
+
+    <div class="inner">
+    
+    <section>
+
+    <h2>趣味、特技</h2>
+        
+    <h3>サッカー</h3>
+
+    <h3>旅行</h3>
+
+    <h3>バー巡り</h3>
+
+    <h3>昭和について学ぶこと</h3>
+
+    <h3>ルービックキューブ</h3>
+
+    <h3>ドライブ</h3>
+    
+
+    </section>
+    
+    
+    </div>
+    <!--/.inner-->
+
+</div>
+<!--/.contents-->
+
+<div class="contents" id="gallery">
+
+    <div class="inner">
+    
+    <section>
+
+    <h2>写真</h2>
+    
+
+    <div class="list">
+    <a href="images_photo/hawaii.jpeg" data-lightbox="group1" data-title="htmlのdata-title部分で説明を入れる事ができます">
+    <figure><img src="images/hawaiianfriends.jpg" alt="ハワイの友達"></figure>
+    </a>
+    </div>
+
+    <div class="list">
+    <a href="images_photo/santamonica.jpeg" data-lightbox="group1" data-title="htmlのdata-title部分で説明を入れる事ができます">
+    <figure><img src="images_photo/santmonica.jpeg" alt="サンタモニカのサンセット"></figure>
+    </a>
+    </div>
+
+    <div class="list">
+    <a href="images_photo/vegasmovie.mov" data-lightbox="group1" data-title="htmlのdata-title部分で説明を入れる事ができます">
+    <figure><img src="images_photo/vegas1.jpeg" alt="ベガス"></figure>
+    </a>
+    
+    </div>
+
+    <div class="list">
+    <a href="images_photo/texashorse2.jpeg" data-lightbox="group1" data-title="htmlのdata-title部分で説明を入れる事ができます">
+    <figure><img src="images_photo/texas.jpg" alt="テキサス"></figure>
+    </a>
+    </div>
+
+    <div class="list">
+    <a href="images_photo/newyorkcity.jpeg" data-lightbox="group1" data-title="htmlのdata-title部分で説明を入れる事ができます">
+    <figure><img src="images_photo/timezsquea.jpeg" alt="ニューヨーク"></figure>
+    </a>
+    </div>
+
+    <div class="list">
+    <a href="images_photo/statueofliberty.jpeg" data-lightbox="group1" data-title="htmlのdata-title部分で説明を入れる事ができます">
+    <figure><img src="images_photo/solsquea.jpeg" alt="自由の女神"></figure>
+    </a>
+    </div>
+
+    <div class="list">
+    <a href="images_photo/hollywoodcar.jpeg" data-lightbox="group1" data-title="htmlのdata-title部分で説明を入れる事ができます">
+    <figure><img src="images_photo/hollywood.jpeg" alt="ロサンゼルス"></figure>
+    </a>
+    </div>
+
+    <div class="list">
+    <a href="images_photo/texashorse.jpeg" data-lightbox="group1" data-title="htmlのdata-title部分で説明を入れる事ができます">
+    <figure><img src="images_photo/texas2.jpeg" alt="テキサスの高速道路"></figure>
+    </a>
+    </div>
+
+    <div class="list">
+    <a href="images_photo/hollywood3.jpeg" data-lightbox="group1" data-title="htmlのdata-title部分で説明を入れる事ができます">
+    <figure><img src="images_photo/hollywood2.jpeg" alt="ハリウッドストリート"></figure>
+    </a>
+    </div>
+
+    </section>
+
+    </div>
+    <!--/.inner-->
+
+</div>
+<!--/.contents-->
+
+<div class="contents bg1" id="link">
+
+    <div class="inner">
+    
+    <section>
+
+    <h2>Link<span>リンク</span></h2>
+    
+    <table class="ta1">
+    <caption>自己紹介サイト</caption>
+    <tr>
+    <th><a href="#">初めて作ったサイト</a></th>
+    <td>"file:///Users/kazuyayamamoto/Desktop/test/Kreative101/index.html#journal"></td>
+    </tr>
+    </table>
+
+    </section>
+
+    </div>
+    <!--/.inner-->
+
+</div>
+<!--/.contents-->
+
+<div class="contents bg2" id="contact">
+
+    <div class="inner">
+    
+    <section>
+
+    <h2>連絡先</h2>
+    <p class="c"><span class="big1">ykazuya2137@icloud.com</span></p>
+    <p class="c">そこを右に曲がってあっこを曲がってあっちに行ったらあります</p>
+    <p class="c"><img src="images/dummy_map_main.jpg" alt="" class="ws"><br>
+    </p>
+    
+    </section>
+
+    </div>
+    <!--/.inner-->
+
+</div>
+<!--/.contents-->
+
+<!--lightbox用jsファイル読み込み-->
+
+</body>
+</html>
